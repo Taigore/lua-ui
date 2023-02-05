3669644782d05870c4441983bc2feb687e6621d3
Federico Bellia <f.bellia@hotmail.it>
2023-02-05 23:40:18 +0100

    Minimum project setup

---
+++ LuaUi2.Win32/WinMain.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prevInstance, _In_ LPSTR cmdLine, _In_ int cmdShow) {
+    UNREFERENCED_PARAMETER(prevInstance);
+    UNREFERENCED_PARAMETER(cmdLine);
+    UNREFERENCED_PARAMETER(cmdShow);
+
+    return 0;
+}
